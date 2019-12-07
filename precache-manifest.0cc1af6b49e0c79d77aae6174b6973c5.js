d1232b6e00519ea7fd86b3a2f99786bfbc2c97ad
Daiki Okumura <daiki.okumura@gmail.com>
2019-12-07 15:54:10 +0900

    Updates

---
+++ precache-manifest.0cc1af6b49e0c79d77aae6174b6973c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeabda6dc8a86772cd6a6984121bbf0b",
+    "url": "/shake-janken/index.html"
+  },
+  {
+    "revision": "869c48dcfd94cad38d49",
+    "url": "/shake-janken/static/css/main.a72a6848.chunk.css"
+  },
+  {
+    "revision": "676fc696e0cf2f3dc38d",
+    "url": "/shake-janken/static/js/2.5d324d0d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shake-janken/static/js/2.5d324d0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "869c48dcfd94cad38d49",
+    "url": "/shake-janken/static/js/main.45f70eea.chunk.js"
+  },
+  {
+    "revision": "27eef20894c45ac4f52e",
+    "url": "/shake-janken/static/js/runtime-main.2787399a.js"
+  }
+]);
